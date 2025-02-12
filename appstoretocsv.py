d33dce49ca77b99aa03c98871cdd3c33a2b4cd70
austindeanpowell <167955520+austindeanpowell@users.noreply.github.com>
2025-02-11 21:58:30 -0800

    Update appstoretocsv.py

--- appstoretocsv.py
+++ appstoretocsv.py
@@ -4,8 +4,10 @@ import csv
 
 # Define the app (Co-Star example)
 co_star = AppStore(country="us", 
+                   #Can chage sort.NEWEST with other functions to analyze
                    sort=Sort.NEWEST,
-                   app_name="co-star-personalized-astrology", 
+                   app_name="co-star-personalized-astrology",
+                   # Within the App URL from Browser the app_id will be the last 10 digits within the URL
                    app_id=1264782561)
 
 # Fetch reviews (limit to 50)
@@ -17,4 +19,5 @@ df = pd.DataFrame(co_star.reviews)
 # Save to CSV
 df.to_csv("co_star_reviews.csv", index=False, encoding="utf-8")
 
+#Will save locally into VSCODE or device when code Runs. Spotlight search on device if unable to find
 print("✅ Data saved as co_star_reviews.csv")
